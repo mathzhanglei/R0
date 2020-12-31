1f65d6021a830083e5cf14917a3c27e933435f19
mathzhanglei <zhanglei890512@gmail.com>
2020-12-31 18:01:13 -0330

    PT rightarrow T

--- Examples/Example3/ode45/solveode.m
+++ Examples/Example3/ode45/solveode.m
@@ -6,9 +6,9 @@ C=[0 0 1];%%%%% in this program, it is useless
 Data=init();
 v=[0 1]';
 mu=1;
-PT=Data.PT;
-[PT,v] = ode15s(@(t,y) func(t,y,mu,PT),[0 PT],v);
-v(end,:)'
+T=Data.T;
+[TT,VV] = ode15s(@(t,y) func(t,y,mu,T),[0 T],v);
+VV(end,:)'
 % v=[0 1]';
 % [PT,v] = ode15s(@(t,y) func(t,y,mu,PT),[0 PT],v);
 % v(end,:)'
